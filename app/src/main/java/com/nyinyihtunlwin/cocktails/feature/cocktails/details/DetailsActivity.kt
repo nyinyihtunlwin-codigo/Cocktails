aa30150312658ff12fd404cc80cf4582c1e0f41c
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2019-07-04 16:59:55 +0630

    Details UI added.

---
+++ app/src/main/java/com/nyinyihtunlwin/cocktails/feature/cocktails/details/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.nyinyihtunlwin.cocktails.feature.cocktails.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nyinyihtunlwin.cocktails.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
